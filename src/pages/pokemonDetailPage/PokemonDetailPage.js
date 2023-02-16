380297ef88e25c1cd20c7407129f95cefe4ce128
Kieffer Torricilia <kieffer.torricilia@gmail.com>
2023-02-16 20:48:02 -0300

    Estilizacao da pagina de detalhes

--- src/pages/pokemonDetailPage/PokemonDetailPage.js
+++ src/pages/pokemonDetailPage/PokemonDetailPage.js
@@ -1,32 +1,78 @@
-import { Titulo, Container, ContainerDetalhes, BoxImagem, BaseStats, Imagem } from "./Styled";
+import {
+    Titulo, Container, ContainerDetalhes, BoxImagem, BaseStats, Imagem, Id, Name, Type, Type2,
+    ImagemGrande, PokebolaDeFundo, Moves, TituloMoves, Movimentos
+} from "./Styled";
 import Header from '../../components/header/Header'
 import { useContext } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { GlobalContext } from "../../contexts/GlobalContext";
+import Grass from '../../assets/Grass.png'
+import Bug from '../../assets/Bug.png'
+import Fire from '../../assets/Fire.png'
+import Fly from '../../assets/Fly.png'
+import Normal from '../../assets/Normal.png'
+import Poison from '../../assets/Poison.png'
+import Water from '../../assets/Water.png'
+import PokebolaGrande from '../../assets/PokebolaGrande.png'
 
-export default function PokemonDetailPage () {
+
+
+export default function PokemonDetailPage() {
 
     const context = useContext(GlobalContext)
-    const {detalhes} = context
+    const { detalhes } = context
     console.log(detalhes)
 
-    return(
+    const imagem = (type) => {
+        switch (type) {
+            case "grass":
+                return Grass;
+            case "fire":
+                return Fire;
+            case "water":
+                return Water;
+            case "bug":
+                return Bug;
+            case "normal":
+                return Normal;
+            case 'flying':
+                return Fly;
+            case 'poison':
+                return Poison;
+        }
+    }
+
+    return (
         <>
-        <Header/>
-        <Container>
-        <Titulo>Detalhes</Titulo>
-        <ContainerDetalhes>
-            <BoxImagem>
-            <Imagem src={detalhes.frontDetail} />
-            </BoxImagem>
-            <BoxImagem>
-            <Imagem src={detalhes.backDetail} />
-            </BoxImagem>
-            <BaseStats>
-            Estatus
-            </BaseStats>
-        </ContainerDetalhes>
-        </Container>
+            <Header />
+            <Container>
+                <Titulo>Detalhes</Titulo>
+                <ContainerDetalhes>
+                    <BoxImagem>
+                        <Imagem src={detalhes.frontDetail} />
+                    </BoxImagem>
+                    <BoxImagem>
+                        <Imagem src={detalhes.backDetail} />
+                    </BoxImagem>
+                    <BaseStats>
+                        Estatus
+                    </BaseStats>
+                    <Id> #{detalhes.id} </Id>
+                    <Name> {detalhes.name} </Name>
+                    <Type variant={detalhes.type[0]}> <img src={imagem(detalhes.type[0])} />  {detalhes.type[0]} </Type>
+                    <Type2 variant={detalhes.type[1]}> <img src={imagem(detalhes.type[1])} />  {detalhes.type[1]} </Type2>
+                    <ImagemGrande src={detalhes.img} />
+                    <PokebolaDeFundo src={PokebolaGrande} />
+                    <Moves>
+                        <TituloMoves>Moves:</TituloMoves>
+                        <Movimentos> {detalhes.moves[0]}  </Movimentos>
+                        <Movimentos> {detalhes.moves[1]}  </Movimentos>
+                        <Movimentos> {detalhes.moves[2]}  </Movimentos>
+                        <Movimentos> {detalhes.moves[3]}  </Movimentos>
+                    </Moves>
+                    
+                </ContainerDetalhes>
+            </Container>
         </>
     )
 }
--- src/pages/pokemonDetailPage/Styled.js
+++ src/pages/pokemonDetailPage/Styled.js
@@ -1,6 +1,26 @@
 import styled from "styled-components";
 
 
+const corTipo = (cor) => {
+
+    switch(cor){
+        case "grass":
+            return '#70B873';
+        case "fire":
+            return '#F44900'
+        case "water":
+            return '#33A4F5'
+        case "bug":
+            return '#316520'
+        case "normal":
+            return '#8A8A8A'
+        case 'flying':
+            return '#6892B0'
+        case 'poison':
+            return '#AD61AE'
+    }
+}
+
 export const Titulo = styled.h1`
 position: absolute;
 width: 220px;
@@ -71,3 +91,183 @@ right: 250px;
 background: #FFFFFF;
 border-radius: 12px;
 `
+
+export const Id = styled.div`
+position: absolute;
+width: 30px;
+height: 19px;
+left: 774px;
+top: 24px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 700;
+font-size: 16px;
+line-height: 19px;
+
+color: #FFFFFF;
+`
+
+export const Name = styled.div`
+position: absolute;
+width: 238px;
+height: 58px;
+left: 771px;
+top: 39px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 700;
+font-size: 48px;
+line-height: 58px;
+color: #FFFFFF;
+`
+export const Type = styled.span`
+color: white;
+
+text-transform: capitalize;
+
+border: 1px solid #FFFFFF78;
+
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 5px 8px;
+gap: 17px;
+
+position: absolute;
+width: 80px;
+height: 21px;
+left: 772px;
+top: 100px;
+
+background: ${(props) => corTipo(props.variant) };
+border: 1px dashed rgba(255, 255, 255, 0.47);
+border-radius: 8px;
+
+//Letras
+font-family: Poppins;
+font-size: 14px;
+font-weight: 400;
+line-height: 21px;
+letter-spacing: 0em;
+text-align: left;
+`
+
+export const Type2 = styled.span`
+
+border: 1px solid #FFFFFF78;
+
+text-transform: capitalize;
+
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 5px 8px;
+gap: 17px;
+
+position: absolute;
+width: 80px;
+height: 21px;
+left: 885px;
+top: 100px;
+
+background: ${(props) => corTipo(props.variant)};
+border: ${(props) => props.variant === undefined ? "none" : "1px dashed rgba(255, 255, 255, 0.47)"};
+border-radius: 8px;
+
+//Letras
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 21px;
+/* identical to box height */
+
+
+color: #FFFFFF;
+`
+
+export const ImagemGrande = styled.img`
+position: absolute;
+width: 270px;
+height: 270px;
+
+left: 1000px;
+bottom: 545px;
+`
+
+export const PokebolaDeFundo = styled.img`
+height: 675px;
+width: 650px;
+left: 644px;
+
+border-radius: 0px;
+
+position: absolute;
+`
+
+export const Moves = styled.div`
+display: flex;
+flex-direction: column;
+gap: 20px;
+flex-wrap: wrap;
+
+position: absolute;
+width: 292px;
+height: 463px;
+left: 771px;
+top: 184px;
+
+background: #FFFFFF;
+border-radius: 8px;
+`
+
+export const TituloMoves = styled.div`
+position: absolute;
+left: 18px;
+top: 10px;
+font-family: 'Inter';
+font-style: normal;
+font-weight: 800;
+font-size: 24px;
+line-height: 29px;
+width: 87px;
+height: 29px;
+
+`
+export const Movimentos = styled.div`
+
+/* align-items: center; */
+/* justify-content: center; */
+padding: 10px;
+align-items: flex-start;
+gap: 10px;
+
+position: relative;
+top: 50px;
+left: 15px;
+width: 100px;
+height: 27px;
+
+
+background: #ECECEC;
+
+border: 1px dashed rgba(0, 0, 0, 0.14);
+border-radius: 12px;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 450;
+font-size: 17px;
+line-height: 25px;
+color: #000000;
+
+
+/* identical to box height */
+
+
+
+`
+
+
+
